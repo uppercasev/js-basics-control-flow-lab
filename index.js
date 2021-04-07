add0c069546d7048c40dae2172dce2cd08395b4b
Hanaa <hsadoqi@gmail.com>
2021-04-07 16:58:18 -0400

    commit tree 2710b4db5b22eaa16492ea671b404227e32e704d

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+  if(ride <= 400){
+    return 'This one is on me!'
+  } else if(ride > 2000 && ride < 2500){
+    return 'I will gladly take your thirty bucks.'
+  } else {
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  // if(city === 'NYC'){
+  //   return "Ok, sounds good."
+  // } else {
+  //   return "No go."
+  // }
+  // city === 'NYC' ? 'Ok, sounds good.' : "No go."
+
+  return city === 'NYC' ? 'Ok, sounds good.' : "No go."
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+
+  switch (tip) {
+    case 'generous': 
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+    default: 
+      return 'Bye.'
+  }
+
 }
